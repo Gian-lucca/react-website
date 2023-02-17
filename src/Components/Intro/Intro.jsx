f9bd0dd810070419265179608ca3e5a902818688
Gianlucca Augusto <89024507+Gian-lucca@users.noreply.github.com>
2023-02-17 03:42:41 -0300

    Intro / Serviços

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -35,9 +35,15 @@ const Intro = () => {
            <img src={Vector1} alt="" />
            <img src={Vector2} alt="" />
            <img src={boy} alt="" />
-           <div>
+           <img src={glassesimoji} alt="" />
+           <div style={{top: '-4%', left: '60%'}}>
             <FloatingDiv image={Crown} txt1='Web' txt2='Developer'/>
            </div>
+           <div style={{top:'18rem', left: '0rem'}}>
+            <FloatingDiv image={thumbup} txt1='Soluções confiáveis' txt2={'e seguras'}/>
+           </div>
+           <div className="blur" style={{background: "rgb(238 210 255)"}}></div>
+           <div className="blur" style={{background: '#C1F5FF', top: '17rem', width:'21rem', height:'11rem', left:'-9rem'}}></div>
         </div>
 
     </div>
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
 import './services.css'
+import Card from '../Card/Card'
+import HertEmoji from "../../img/heartemoji.png"
+import Glasses from "../../img/glasses.png"
+import Humble from "../../img/humble.png"
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className="services">
+        <div className="awersome">
+          <span>Meu incrível</span>
+          <span>Serviço</span>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa atque ab architecto 
+            <br/>
+            cumque necessitatibus modi explicabo commodi eaque nobis quaerat!
+          </span>
+          <button className="button s-button">Download CV</button>
+          <div className="blu s-blur1" style={{ background: "#ABF1FF94"}}></div>
+        </div>
+
+        <div className="div cards">
+          <div>
+            <Card emoji = {HertEmoji} heading = {'Desenvolvedor Front end'} detail = {"HTML, CSS, Drupal, JavaScript, React, BootsTrap, TypeScript"}/>
+          </div>
+        </div>
+    </div>
   )
 }
 
