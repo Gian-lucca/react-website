b3ab25a9aad3a48204c1f6c08ff00ec64baecbdd
Gianlucca Augusto <89024507+Gian-lucca@users.noreply.github.com>
2023-02-17 23:13:09 -0300

    Serviços / Experiencias

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -3,7 +3,27 @@ import './experience.css'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div className="experience">
+
+      <div className="anchievement">
+        <div className="circle">+2</div>
+        <span>Anos</span>
+        <span>Experiencia</span>
+      </div>
+
+      <div className="anchievement">
+        <div className="circle">+150</div>
+        <span>Completos</span>
+        <span>Projetos</span>
+      </div>
+
+      <div className="anchievement">
+        <div className="circle">+100</div>
+        <span>Clientes</span>
+        <span>Atendidos</span>
+      </div>
+
+    </div>
   )
 }
 
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -4,6 +4,7 @@ import Card from '../Card/Card'
 import HertEmoji from "../../img/heartemoji.png"
 import Glasses from "../../img/glasses.png"
 import Humble from "../../img/humble.png"
+import Resume from '../Services/resume.pdf'
 
 const Services = () => {
   return (
@@ -16,15 +17,29 @@ const Services = () => {
             <br/>
             cumque necessitatibus modi explicabo commodi eaque nobis quaerat!
           </span>
-          <button className="button s-button">Download CV</button>
-          <div className="blu s-blur1" style={{ background: "#ABF1FF94"}}></div>
-        </div>
 
-        <div className="div cards">
-          <div>
-            <Card emoji = {HertEmoji} heading = {'Desenvolvedor Front end'} detail = {"HTML, CSS, Drupal, JavaScript, React, BootsTrap, TypeScript"}/>
+          <a href={Resume} download>
+            <button className="button s-button">Download CV</button>
+          </a>
+          
+          
+        </div>
+        <div className="blu s-blur1" style={{ background: "#ABF1FF94"}}></div>
+        <div className="cards">
+
+          <div style={{left: '14rem'}}>
+            <Card emoji = {HertEmoji} heading = {'Tecnologias  Front end'} detail = {"HTML, CSS, Drupal, JavaScript, React, BootsTrap, TypeScript"}/>
+          </div>
+
+          <div style={{ top: "12rem", left: "-4rem"}}>
+            <Card emoji = {Glasses} heading = {'Tecnologias  Back end'} detail = {"PHP, MySQL"}/>
+          </div>
+
+          <div style={{ top: "19rem", left: "12rem"}}>
+            <Card emoji = {Humble} heading = {'Designer'} detail = {"Photoshop"}/>
           </div>
         </div>
+        <div className="blur s-blur2" style={{ background: "var(--purple)"}}></div>
     </div>
   )
 }
